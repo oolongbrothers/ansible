5d5ee0c71162626df63580fe4a7f21f83798aa34
Alexander Bethke <oolongbrothers@gmx.net>
2018-04-08 13:51:42 +0200

    Adds me to the copyright notice as well :) (#28808)

--- lib/ansible/modules/packaging/os/flatpak_remote.py
+++ lib/ansible/modules/packaging/os/flatpak_remote.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2017 John Kwiatkoski
+# Copyright: (c) 2018 Alexander Bethke
 # Copyright: (c) 2017 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
